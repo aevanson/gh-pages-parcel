e215b788a84183cc865b28ac522161d13e9291ab
Alan Evanson <alan.evanson@sapientrazorfish.com>
2019-03-20 09:39:08 -0400

    Updates

--- scripts.7ada455b.js
+++ scripts.7ada455b.js
@@ -1869,7 +1869,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54180" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50604" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
